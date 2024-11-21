606eb1596069116db8f7869c6ebccb668ea2c76d
Binhnv <binh46607@gmail.com>
2024-11-21 21:31:22 +0700

    update

--- src/server.js
+++ src/server.js
@@ -16,18 +16,20 @@ const io = new Server(server, {
 
 // Đọc dữ liệu từ file data.json
 const dataFilePath = path.join(__dirname, 'data.json');
+const dataScanFilePath = path.join(__dirname, 'scan-data.json');
 let sensorData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
+let scanDataMock = JSON.parse(fs.readFileSync(dataScanFilePath, 'utf8'));
 
 // Cấu hình CORS cho API (dành cho HTTP)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Cho phép domain http://localhost:3000 truy cập
-  res.header('Access-Control-Allow-Methods', 'GET, POST'); // Cho phép các phương thức GET và POST
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE'); // Cho phép các phương thức GET và POST
   res.header('Access-Control-Allow-Headers', 'Content-Type'); // Cho phép header Content-Type
   next();
 });
 
 // API endpoint trả về dữ liệu sensor
-app.get('/api/sensors', (req, res) => {
+app.get('/api/v2/sensor/all', (req, res) => {
   res.json(sensorData); // Trả về dữ liệu sensor dưới dạng JSON
 });
 
@@ -47,6 +49,47 @@ io.on('connection', (socket) => {
     }
   });
 
+  app.delete('/api/v2/sensor/:id', (req, res) => {
+    const sensorId = req.params.id;
+
+    // Tìm và xóa sensor trong dữ liệu
+    const updatedSensorData = sensorData.data.filter(
+      (sensor) => sensor.id !== sensorId
+    );
+
+    sensorData = { ...sensorData, data: updatedSensorData };
+
+    if (updatedSensorData.length === sensorData.length) {
+      return res.status(404).json({ error: 'Sensor not found' });
+    }
+
+    // Ghi lại dữ liệu đã cập nhật vào file
+    fs.writeFileSync(dataFilePath, JSON.stringify(sensorData, null, 2));
+    sensorData = updatedSensorData; // Cập nhật dữ liệu sensor trong bộ nhớ
+
+    // Phản hồi thành công
+    res.status(200).json({
+      message: 'Sensor deleted successfully',
+      data: updatedSensorData,
+    });
+
+    // Phát tín hiệu cập nhật qua WebSocket
+    io.emit('sensorData', sensorData);
+  });
+
+  // Lắng nghe sự kiện startSensorScan từ client
+  socket.on('startSensorScan', (sensorId) => {
+    console.log(`Start scan for sensor ID: ${sensorId}`);
+
+    // Thực hiện logic xử lý: Gửi dữ liệu hoặc bắt đầu quá trình scan
+    socket.emit(`sensor/${sensorId}/scanResultData`, scanDataMock); // Gửi lại dữ liệu scan tới client
+  });
+
+  // Lắng nghe sự kiện stopSensorScan từ client
+  socket.on('stopSensorScan', (sensorId) => {
+    console.log(`Stop scan for sensor ID: ${sensorId}`);
+    // Dừng scan hoặc ngắt kết nối liên quan đến sensorId (nếu cần)
+  });
   // Xử lý khi client ngắt kết nối
   socket.on('disconnect', () => {
     console.log('Client disconnected:', socket.id);
